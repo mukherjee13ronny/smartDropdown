5bea2eacd2385a83ad40544d8d2ae5e430c623c3
unknown <mukherjee13ronny@gmail.com>
2020-08-17 16:38:26 +0530

    Updates

---
+++ precache-manifest.c26c1346ad6a85052384b6203e0b211b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b46d13d24f3f10cfe7ce",
+    "url": "/smartDropdown/static/js/runtime~main.1e923ed6.js"
+  },
+  {
+    "revision": "abd8bf0d457f02b630d4",
+    "url": "/smartDropdown/static/js/main.9619f06f.chunk.js"
+  },
+  {
+    "revision": "69ed2c955e72165dd0cd",
+    "url": "/smartDropdown/static/js/2.2e8049d9.chunk.js"
+  },
+  {
+    "revision": "abd8bf0d457f02b630d4",
+    "url": "/smartDropdown/static/css/main.7890f7ce.chunk.css"
+  },
+  {
+    "revision": "dd9d2b1067f33e77bde82e075eef51c8",
+    "url": "/smartDropdown/index.html"
+  }
+];
